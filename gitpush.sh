0ee1c37354a6c6a05802e6763c99f5ac91cb8ae3
oneleaf <oneleaf@gmail.com>
2022-07-15 14:21:48 +0800

    add github sh

---
+++ gitpush.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 进行git的函数，处理从add到push
+function gitpush()
+{
+    error_str1="fatal"
+    error_str2="error"
+
+    # push
+    while true
+    do
+        echo "***开始push本地仓库***"
+        var=$(git push origin master:master 2>&1)
+        if [[ $var =~ $error_str1 || $var =~ $error_str2 ]]; then 
+            echo "***push远程仓库出现错误***"
+            echo $var
+        elif [[ $var =~ "git pull" ]]; then 
+            echo "***pull远程仓库***"
+            var=$(git pull 2>&1)
+            echo $var
+        else
+            echo $var
+            echo "***push完成***"
+            break
+        fi
+    done
+}
+
+gitpush
