34185333f034a6b8ba6fc4170f1411d93b5ff7a0
oneleaf <oneleaf@gmail.com>
2022-07-15 16:10:28 +0800

    .

--- run/portainer.sh
+++ run/portainer.sh
@@ -6,9 +6,9 @@ docker run -d \
     --name $NAME \
     --restart=always \
     --memory="256m" \
-    --cpus="1.0" \    
+    --cpus="1.0" \
     -p $IP:9000:9000 \
-    -e TZ=PRC \    
+    -e TZ=PRC \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     docker.io/portainer/portainer
